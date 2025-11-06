751c50d1ac6ac0efd368f98b28bcc269999d2ecb
Dax Manuel <dax.manuel@unb.ca>
2025-11-05 20:12:20 -0400

    initialized spring boot

---
+++ src/main/java/com/example/cs2043/Cs2043Application.java
@@ -0,0 +1,13 @@
+package com.example.cs2043;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs2043Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cs2043Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cs2043/Cs2043ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cs2043;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs2043ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
