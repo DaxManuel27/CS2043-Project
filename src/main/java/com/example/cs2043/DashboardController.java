ec6a7788d4e7be295874c124fc318e55ebe63348
Ugochukwu James Ukandu <jamesukandunew@gmail.com>
2025-11-20 21:42:46 -0400

    fixed admin count on dashboard controller

--- src/main/java/com/example/cs2043/DashboardController.java
+++ src/main/java/com/example/cs2043/DashboardController.java
@@ -1,5 +1,6 @@
 package com.example.cs2043;
 
+import com.example.cs2043.Entities.Administrator;
 import com.example.cs2043.Repositories.EmployeeRepository;
 import com.example.cs2043.Repositories.LeaveRequestRepository;
 import com.example.cs2043.Entities.Employee;
@@ -27,21 +28,41 @@ public class DashboardController {
 
     @GetMapping("/admins")
     public List<Employee> admins() {
-        return List.of();
+        List<Employee> all = employeeRepo.findAll();
+        List<Employee> admins = new ArrayList<>();
+
+        for (int i = 0; i < all.size(); i++) {
+            Employee e = all.get(i);
+            if (e instanceof Administrator) {
+                admins.add(e);
+            }
+        }
+
+        return admins;
     }
 
     @GetMapping("/leave-requests")
     public List<LeaveRequest> leaveRequests() {
         return leaveRepo.findAll();
     }
-	
-	@GetMapping("/summary")
-	public Map<String,Object> summary() {
-		Map<String,Object> data = new HashMap<>();
-		data.put("totalEmployees", employeeRepo.count());
-		data.put("totalAdmins", 0);  // you removed admin counting
-		data.put("totalLeaveRequests", leaveRepo.count());
-		return data;
-	}
 
+    @GetMapping("/summary")
+    public Map<String,Object> summary() {
+        List<Employee> all = employeeRepo.findAll();
+        int adminCount = 0;
+
+        for (int i = 0; i < all.size(); i++) {
+            Employee e = all.get(i);
+            if (e instanceof Administrator) {
+                adminCount++;
+            }
+        }
+
+        Map<String,Object> data = new HashMap<>();
+        data.put("totalEmployees", all.size());
+        data.put("totalAdmins", adminCount);
+        data.put("totalLeaveRequests", leaveRepo.count());
+
+        return data;
+    }
 }
